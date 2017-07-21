bf75f2c5579a741efa17558c6a24ef770ff3facb
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-20 22:30:27 -0700

    add styles

--- score-keep/imports/ui/titleBar.js
+++ score-keep/imports/ui/titleBar.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 export default class TitleBar extends React.Component{
     render(){
         return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
+            <div className="title-bar">
+                <div className="wrapper">
+                    <h1>{this.props.title}</h1>
+                    {this.props.subtitle}
+                </div>
             </div>
         )
     }
