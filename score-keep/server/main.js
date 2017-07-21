a6ebdc014c39891b692c554466957b73091894f2
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-20 17:17:27 -0700

    update

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -1,11 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
-import { add } from '../imports/math';
-
-console.log();
+// App modules
+import { Players } from '../imports/api/players';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  
 });
 
-console.log(add(3,5));
